0f590b6bb6a9cf7d078371844507ad57beb8168a
kisaragisyuuji <kairasuof03@yahoo.co.jp>
2019-11-22 20:59:54 +0900

    自動更新
    自動更新と
    メモ書き削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
     function buildHTML(message){
         var messageImage = (message.image !== null) ? `<img class ="image_size", src="${message.image}">` : ""
-        
-        var html = `
+         
+        var html = `<div class="message" data-message-id="${message.id}">
           <p class = "upper-user-name">
             ${message.user_name}
           </p>
@@ -14,13 +14,15 @@ $(function(){
           </p>
           <p class = "user-image">
             ${messageImage}
-          </p>`
+          </p>
+        </div>`
         return html;
     }
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
+
     $.ajax({
       url: url,
       type: "POST",
@@ -41,4 +43,31 @@ $(function(){
         $('.submit').prop('disabled', false);
       })
   })
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      last_message_id = $('.message:last').data("message-id");
+        console.log(last_message_id);
+      $.ajax({
+        url : "api/messages",
+        type: "GET",
+        dataType: 'json',
+        data: {last_id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+          messages.forEach(function (message){
+          insertHTML = buildHTML(message);
+          $('.mainmessage').append(insertHTML);
+          })
+          $('.mainmessage').animate({scrollTop: $('.mainmessage')[0].scrollHeight}, 'fast');
+            console.log("成功");
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+        console.log("失敗");
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000); 
+  
 });
