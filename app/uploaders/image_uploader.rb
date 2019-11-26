a515de5dd46b07c8fd0d14d00825c4e5bff62502
kisaragisyuuji <kairasuof03@yahoo.co.jp>
2019-11-26 11:47:57 +0900

    デプロイ変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,10 +2,11 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-  storage :file
+  storage :fog
   process resize_to_fit: [800, 800]
   process convert: 'jpg'
 
+  
   # 保存するディレクトリ名
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
